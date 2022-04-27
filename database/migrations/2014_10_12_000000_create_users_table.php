27e0f7c6668ed68b5d0129421110f0da68feb9aa
lutfiaar <lutfiaariyati@gmail.com>
2022-04-27 13:00:17 +0700

    isi tabel users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,14 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('cust_id');
+            $table->unsignedBigInteger('dept_id');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('role_id')
+                  ->references('id')->on('roles')
+                  ->onDelete('cascade');
         });
     }
 
