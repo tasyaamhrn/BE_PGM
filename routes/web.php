b5288ebc175b96142e78f5c80cee8d56fb2f59ce
tasya <tasyamaharani77@gmail.com>
2022-05-17 22:23:04 +0700

    template login

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/addemployee',[EmployeeController::class,'register']);
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('auth.login');
 });
